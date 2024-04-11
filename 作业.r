528d76628c309a33f19f8bcf16e42107f85e1c9a
Ying Zha <151952519+zhazhadz@users.noreply.github.com>
2024-04-11 14:06:59 +0800

    Update 1.r

--- 作业.r
+++ 作业.r
@@ -18,9 +18,6 @@ dv_zone2_sf <- st_as_sf(dv_zone2)
 df <- read.table("hefei.txt", header = TRUE, sep = "\t")
 
 
-
-
-
 # 将数据框转换为sf对象
 points <- st_as_sf(df, coords = c("lng", "lat"), crs = 4326)
 
@@ -33,17 +30,19 @@ zones <- list(dv_zone1, dv_zone2)
 
 distances1 <- map(zones, ~st_distance(points, dv_zone1))
 
-print(lapply(distances, head, 5))
-distances2 <- map(zones, ~st_distance(points, dv_zone2))
+# 将distances1转换为数据框
+distances_f1 <- do.call(rbind, distances1)
 
-# 计算distances1为0的元素的个数
-zero_counts1<- lapply(distances1, function(x) sum(x == set_units(0, units(x))))
+# 计算为0的结果的个数
+zero_counts1 <- sum(distances_f1 == 0)
+
+distances2 <- map(zones, ~st_distance(points, dv_zone2))
+# 将distances1转换为数据框
+distances_f2 <- do.call(rbind, distances2)
+
+# 计算为0的结果的个数
+zero_counts2 <- sum(distances_f2 == 0)
 
 # 打印结果
 print(zero_counts1)
-
-# 计算distances1为0的元素的个数
-zero_counts2<- lapply(distances1, function(x) sum(x == set_units(0, units(x))))
-
-# 打印结果
 print(zero_counts2)
